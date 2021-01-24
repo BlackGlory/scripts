866b57c3033cc812084e2a613061ff17e293ddea
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-24 14:57:12 +0000

    feat: add powershell scripts

---
+++ powershell/convert-flv-to-mp4.ps1
@@ -0,0 +1,46 @@
+# 使用 ffmpeg 将 flv 文件转换为 mp4, 在同目录下以 mp4 扩展名保存
+# convert-flv-to-mp4.ps1 [-filename] <string> [[-title] <string>]
+# filename 源文件路径, 通常为扩展名为 flv 的文件
+# title 输出文件名的前缀, 可以省略
+
+param (
+  [Parameter(Mandatory=$true)]
+  [String]
+  $filename = $(throw "filename parameter is required.")
+, [String]
+  $title
+)
+
+function New-Filename($basename, $extension = '', $title = '') {
+  $result = $basename + $extension
+  if ($title) {
+    $result = $title+ ' ' + $result
+  }
+  return $result
+}
+
+function Get-File($filename) {
+  return Get-Item $filename
+}
+
+function Get-Basename($filename) {
+  return (Get-File $filename).BaseName
+}
+
+function Get-Directory($filename) {
+  return (Get-File $filename).Directory
+}
+
+function Get-Extension($filename) {
+  return (Get-File $filename).Extension
+}
+
+$basename = Get-Basename $filename
+$new_filename = New-Filename $basename '.mp4' $title
+$directory = Get-Directory $filename
+$output = Join-Path $directory $new_filename
+
+ffmpeg `
+  -i $filename `
+  -c copy `
+  -f mp4 $output
---
+++ powershell/find-process-by-port.ps1
@@ -0,0 +1,19 @@
+# 查找占用端口的进程
+# find-process-by-port.ps1 [-port] <int>
+# port 需要查询的端口号
+
+param (
+  [Parameter(Mandatory=$true)]
+  [Int]
+  $port = $(throw "port parameter is required.")
+)
+
+function Get-ProcessByPid($pids) {
+  return Get-Process -Id $pids
+}
+
+function Find-ProcessByLocalPort($port) {
+  return (Get-NetTCPConnection -LocalPort $port).OwningProcess | Select-Object -Unique
+}
+
+Write-Output (Get-ProcessByPid (Find-ProcessByLocalPort $port))
